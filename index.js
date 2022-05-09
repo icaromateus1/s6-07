ef882da1fc378b2eea259b8521a6bd6bee34e885
unknown <icaro_300@hotmail.com>
2022-05-09 12:31:51 -0300

    att

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ function add(x,n){
 }
 
 
-function multiply(x,n){
-    let resultado = x * n 
-    return resultado
-}
+function multiply(n){
+    var res = 1
+    for(var i = 0; i < n.length; i++) res *= n[i];
+    return res;
+ }
+
+
 
 function power(x,n){
     let resultado = x ** n 
